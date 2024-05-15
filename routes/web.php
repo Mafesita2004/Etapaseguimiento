39db833e263dc02ba2925e558456974347ad73d1
lolkarmalol <jhodier17q@gmail.com>
2024-05-15 11:56:12 -0500

    Se agrego ruta

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/login', [LoginController::class, 'loginStore'])->name('login.store');
