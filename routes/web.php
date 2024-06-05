fa50c87370cee4856284b1ea61ccaa4774c4fd55
dayanacantero <astridcantero2005@gmail.com>
2024-06-05 10:46:09 -0500

    Tus comentarios sobre los cambios realizados

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeaprendizController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,9 @@ Route::get('/', function () {
 //RUTA LOGIN
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'loginStore'])->name('login.store');
+
+
+//RUTA HOME APRENDIZ
+Route::get('/homeaprendiz', [HomeaprendizController::class, 'showHomeaprendizForm'])->name('Homeaprendiz');
+Route::post('/homeaprendiz', [HomeAprendizController::class, 'homeaprendizStore'])->name('homeaprendiz.store');
+
