c44d4fca46c1aadd255e378e23ec9900ce2534bc
dayanacantero <astridcantero2005@gmail.com>
2024-06-05 11:12:43 -0500

    comentario

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,8 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'loginStore'])->name('login.store');
 
-<<<<<<< HEAD
-
 //RUTA HOME APRENDIZ
 Route::get('/homeaprendiz', [HomeaprendizController::class, 'showHomeaprendizForm'])->name('Homeaprendiz');
 Route::post('/homeaprendiz', [HomeAprendizController::class, 'homeaprendizStore'])->name('homeaprendiz.store');
 
-=======
-//Ruta Registro de aprendizaje que hace el Super admin
->>>>>>> 88d1c6d85282759751b11e707a0d0b153be9d75c
+
